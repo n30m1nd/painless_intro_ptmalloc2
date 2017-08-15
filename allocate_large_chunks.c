084f0cd3f72b1e85d3f39e44c20326ffee0fccd4
Javier Jiménez <javijmor@gmail.com>
2017-08-15 14:51:20 +0100

    Wrong large size and PoC

--- allocate_large_chunks.c
+++ allocate_large_chunks.c
@@ -9,16 +9,20 @@
 int main(){
  void *large1,*large2, *large3; 
 
- printf("[info] allocating 2 large chunks (+1024 bytes each)\n");
- large1 = malloc(1025);
- large2 = malloc(1025);
- large3 = malloc(1025);
+ printf("[info] allocating 4 large chunks (+0x400 bytes each)\n");
+ large1 = malloc(0x3f0);
+ large2 = malloc(0x3f0);
+ large3 = malloc(0x3f0);
+ large4 = malloc(0x3f0);
  printf("[+] large1 at %p\n", large1);
  printf("[+] \tdistance: %ld\n", large2-large1);
  printf("[+] large2 at %p\n", large2);
  printf("[+] \tdistance: %ld\n", large3-large2);
  printf("[+] large3 at %p\n", large3);
- printf("[info] freeing large2\n");
- free(large2);
+ printf("[+] \tdistance: %ld\n", large4-large3);
+ printf("[+] large4 at %p\n", large4);
+ printf("[info] freeing large1 and large2\n");
+ free(large1);
+ free(large3);
  return 0;
 }
